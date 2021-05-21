62b66bedcf0c68a5e9b1d9d98413280c438a7a2b
Ayush Rai <70145826+15Ayush@users.noreply.github.com>
2021-05-22 01:17:08 +0530

    Update eleven.java

--- eleven.java
+++ eleven.java
@@ -4,12 +4,11 @@ class eleven
     public static void main(String [] args)
     {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter two numbers -");
+        System.out.println("Enter a number -");
         int a = input.nextInt();
-        int b = input.nextInt();
-        int c =a/b ;
-        int d = a%b ;
-        System.out.println("Quotient is :- " + c);
-        System.out.println("Remainder is :- " + d);
+        if(a%2==0)
+        System.out.println("Number is Even");
+        else
+        System.out.println("Number is Odd");
     }
 }
